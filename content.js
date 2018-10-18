e1953abab3ce5dc6dcc4b9426156323a9e37a2dc
Lars Huttar <lars@100fold.org>
2018-10-18 00:38:57 -0400

    Get autolink button to start working.

--- content.js
+++ content.js
@@ -8,12 +8,14 @@ chrome.runtime.onMessage.addListener(function (msg, _, sendResponse) {
 });
 
 function autolinkLines(lines) {
-   console.log("text lines", lines.length, text[0].substring(0, 10));
+   console.log("text lines", lines.length, lines[0].substring(0, 10));
    var newLines = [];
    var numLines = lines.length;
-   var wordRegexp = /^[-\w]+$/;
+   // The following requires Unicode property escapes. See
+   // https://stackoverflow.com/a/48902765/423105
+   var wordRegexp = /^[-\p{L}]+$/u;
    var minimumLength = 3; // TODO: get this from extension setting.
-   
+
    for (var i=0; i < numLines; i++) {
       line = lines[i];
 
@@ -26,6 +28,7 @@ function autolinkLines(lines) {
       words = line.split(/\s+/).map(function(word) {
          // Check whether word is really a word (excludes words already linked
          // because "[]" are not allowed.)
+         // TODO: fix the case where multiple words are already within a [[ ]].
          if (word.length > minimumLength && word.match(wordRegexp)) {
             return '[[' + word + ']]';
          } else {
